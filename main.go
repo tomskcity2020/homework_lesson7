8966edf69c0b7ce692558aa6ab9627456300d8b7
tomskcity2020 <tomskcity2020@yandex.ru>
2025-03-25 03:28:12 +0700

    done!

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
-	fmt.Println("test")
+	xy := 4
+	result := ""
+	array := []string{" ", "#"}
+
+	for y := 0; y < xy; y++ {
+
+		for x := 0; x < xy; x++ {
+			key := rand.Intn(2)
+			get_value := array[key]
+			result += get_value
+		}
+
+		result += "\n"
+
+	}
+
+	fmt.Println(result)
 }
